6881f1c429bae8c9782192e6eae32e1334ee5dff
Alex Lancaster <alancast@umich.edu>
2022-02-21 18:37:52 -0800

    Implement fixed point in array

---
+++ python/easy/1064_fixedPoint.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def fixedPoint(self, arr: List[int]) -> int:
+        left = 0
+        right = len(arr) - 1
+        answer = -1
+        # binary search
+        while left <= right:
+            mid = (left + right) // 2
+            num = arr[mid]
+            if num > mid:
+                right = mid - 1
+            elif num < mid:
+                left = mid + 1
+            # A potential answer, now search any answers lower
+            else:
+                right = mid - 1
+                answer = mid
+
+        return answer
+
+testCases = [
+    [[-10,-5,0,3,7], 3],
+    [[0,2,5,8,17], 0],
+    [[-10,-5,3,4,7,9], -1],
+    [[0,1,2,3,4,5], 0],
+    [[-10,-5,-2,0,4,5,6,7,8,9,10], 4]
+]
+implementation = Solution()
+for nums, expected in testCases:
+    answer = implementation.fixedPoint(nums)
+    if answer != expected:
+        print(f"FAILED TEST: Expected {expected} but got {answer}. INPUT: {nums}")
