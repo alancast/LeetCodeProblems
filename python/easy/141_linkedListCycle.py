7251a0b291046bc4dd9a344a0a89e52020c647fc
Alex Lancaster <alancast@umich.edu>
2022-03-08 06:36:06 -0600

    Implement linked list cycle

---
+++ python/easy/141_linkedListCycle.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # Time complexity optimized.
+    # O(n) extra space, min possible runtime (but still O(n))
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        nodes = set()
+
+        while head:
+            if head in nodes:
+                return True
+
+            nodes.add(head)
+            head = head.next
+        
+        return False
+
+    # Space complexity optimized.
+    # O(1) extra space, larger than O(n) time if cycle
+    def hasCycleMinSpace(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        
+        fast = head.next
+        slow = head
+        
+        while fast and fast.next:
+            if fast == slow:
+                return True
+            
+            fast = fast.next.next
+            slow = slow.next
+        
+        return False
