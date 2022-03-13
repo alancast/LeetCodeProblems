41f865676c5debc3d90a3fe2438eebf6d4d479d8
Alex Lancaster <alancast@umich.edu>
2022-03-12 22:13:31 -0800

    Implement valid parenthesis

---
+++ python/easy/20_validParenthesis.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closingsMap = {
+            ']': '[',
+            ')': '(',
+            '}': '{'
+        }
+        
+        stack = []
+        for char in s:
+            if char not in closingsMap:
+                stack.append(char)
+            else:
+                if len(stack) == 0:
+                    return False
+                    
+                lastChar = stack.pop()
+                if closingsMap[char] != lastChar:
+                    return False
+
+        return len(stack) == 0
+
+testCases = [
+    ["", True],
+    ["[]", True],
+    ["(", False],
+    [")", False],
+    ["()[]{}", True],
+    ["([]{})", True],
+    ["(]", False],
+]
+implementation = Solution()
+for s, expected in testCases:
+    answer = implementation.isValid(s)
+    if answer != expected:
+        print(f"FAILED TEST: Expected {expected} but got {answer}. INPUT: {s}")
