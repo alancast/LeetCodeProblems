0f5069ce5712942d68f30a98843338e8bde61a70
Alex Lancaster <alancast@umich.edu>
2025-02-14 13:46:21 -0500

    Implement max ascending subarray sum

---
+++ python/easy/1800_maxAscendingSubarraySum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    # Time O(n) Space O(1)
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        temp_sum = max_sum = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                temp_sum += nums[i]
+                max_sum = max(temp_sum, max_sum)
+            else:
+                temp_sum = nums[i]
+
+        return max_sum
+
+testCases = [
+    [[1, 100, 2, 3, 4], 101],
+    [[10, 20, 30, 40, 50, 1, 100], 150],
+    [[100], 100]
+]
+solution = Solution()
+for nums, expected in testCases:
+    answer = solution.maxAscendingSum(nums)
+    if answer != expected:
+        print(f"FAILED TEST: Expected {expected} but got {answer}. Inputs: {nums}")
+
+print("Ran all tests")
