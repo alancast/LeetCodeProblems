16dda5d3f83d193ca612788d3a60835b384ac39a
Alex Lancaster <alancast@umich.edu>
2025-04-28 21:11:26 -0400

    Implement very easy count subarrays problem

---
+++ python/easy/3392_count_subarrays_condition.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    # Time O(n) as we go over nums once
+    # Space O(1)
+    def countSubarrays(self, nums: List[int]) -> int:
+        count = 0
+        n = len(nums)
+
+        for i in range(n-2):
+            if (2 * (nums[i] + nums[i+2])) == nums[i+1]:
+                count += 1
+
+        return count
+    
+test_cases = [
+    [1, [1,2,1,4,1]],
+    [0, [1,1,1]]
+]
+solution = Solution()
+for expected, nums in test_cases:
+    actual = solution.countSubarrays(nums)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: nums: {nums}")
+
+print("Ran all tests")
