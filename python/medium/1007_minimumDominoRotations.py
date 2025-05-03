7d91d391048b8b277d84ce679a86a7e1ebe6572f
Alex Lancaster <alancast@umich.edu>
2025-05-03 13:40:31 -0400

    Update min swaps to match domino row

--- python/medium/1007_minimumDominoRotations.py
+++ python/medium/1007_minimumDominoRotations.py
@@ -1,34 +1,37 @@
 from typing import List
 
 
-class Solution:        
+class Solution:
+    # Time O(n) as we iterate through top at most twice
+    # Space O(1) as we don't keep any extra
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        # Return min number of swaps if one could make all elements in tops or bottoms equal to x.
-        # returns -1 if not possible
-        def minSwapsToMatch(x: int) -> int:
-            rotations_top = rotations_bottom = 0
-            for i in range(n):
-                # Impossible to match
-                if tops[i] != x and bottoms[i] != x:
-                    return -1
-                elif tops[i] != x:
-                    rotations_top += 1
-                elif bottoms[i] != x:
-                    rotations_bottom += 1
+        rotations = self._min_swaps_to_match(tops[0], tops, bottoms)
 
-            return min(rotations_top, rotations_bottom)
-    
-        n = len(tops)
-        rotations = minSwapsToMatch(tops[0]) 
-        # If top is possible to make match then we know either
-        # 1: bottom will be impossible
-        # 2: bottom will produce the same result as top for min swaps
-        # So return whatever top found
+        # If top and bottom are same number then we have our answer
+        # If they are different numbers but top returned an answer
+        # That means bottom number would require the exact same amount of swaps
+        # So also return to top
         if rotations != -1 or tops[0] == bottoms[0]:
             return rotations 
         # Only needed if top couldn't produce match and top[0] and bottom[0] are different
         else:
-            return minSwapsToMatch(bottoms[0])
+            return self._min_swaps_to_match(bottoms[0], tops, bottoms)
+        
+    # Return min number of swaps if one could make all elements in tops or bottoms equal to x.
+    # returns -1 if not possible
+    def _min_swaps_to_match(self, x: int, tops: List[int], bottoms: List[int]) -> int:
+        n = len(tops)
+        rotations_top = rotations_bottom = 0
+        for i in range(n):
+            # Impossible to match
+            if tops[i] != x and bottoms[i] != x:
+                return -1
+            elif tops[i] != x:
+                rotations_top += 1
+            elif bottoms[i] != x:
+                rotations_bottom += 1
+
+        return min(rotations_top, rotations_bottom)
 
 testCases = [
     [[2,1,2,4,2,2], [5,2,6,2,3,2], 2],
