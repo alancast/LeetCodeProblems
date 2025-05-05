ae9108fe1745c7894cd4e7583ef4e0e37fdd452e
Alex Lancaster <alancast@umich.edu>
2025-05-05 15:15:25 -0400

    implement divide array into equal pairs

---
+++ python/easy/2206_divide_array_equal_pairs.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    # Time O(n) as we go through nums array once
+    # Space O(n) as we keep a set which could be the size of the full array
+    def divideArray(self, nums: List[int]) -> bool:
+        nums_set = set()
+
+        for num in nums:
+            if num in nums_set:
+                nums_set.remove(num)
+            else:
+                nums_set.add(num)
+
+        return len(nums_set) == 0
+    
+test_cases = [
+    [True, [3,2,3,2,2,2]],
+    [False, [1,2,3,4]]
+]
+solution = Solution()
+for expected, nums in test_cases:
+    actual = solution.divideArray(nums)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: nums: {nums}")
+
+print("Ran all tests")
