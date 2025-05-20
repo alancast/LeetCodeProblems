0c11c77dfdab56525f36a15e86eccafe75af0dbf
Alex Lancaster <alancast@umich.edu>
2025-05-20 16:25:58 -0400

    Update partition labels implementation to add runtime and be more readable

--- python/medium/763_partitionLabels.py
+++ python/medium/763_partitionLabels.py
@@ -2,24 +2,29 @@ from typing import List
 
 
 class Solution():
+    # Go through string and find last index, then go through again updating last each time
+    # Time O(n) as we go through string twice
+    # Space O(1) as we store a map of 26 entries
     def partitionLabels(self, s: str) -> List[int]:
-        last = {c: i for i, c in enumerate(s)}
-        j = 0
-        start = 0
-        answer = []
+        # Map of char to last index for it
+        last_index_map = dict()
+        for i, char in enumerate(s):
+            last_index_map[char] = i
 
-        # Range must include last occurrance of this letter
-        # Make sure that all letters in between are included too
-        for i, c in enumerate(s):
-            j = max(j, last[c])
-            if i == j:
-                answer.append(i - start + 1)
+        # Go through and as soon as we get to end of substring append it
+        answer = []
+        start = end = 0
+        for i, char in enumerate(s):
+            end = max(end, last_index_map[char])
+            if i == end:
+                answer.append(end - start + 1)
                 start = i + 1
             
         return answer
 
 testCases = [
     ["ababcbacadefegdehijhklij", [9,7,8]],
+    ["abc", [1,1,1]],
     ["eccbbbbdec", [10]]
 ]
 implementation = Solution()
@@ -27,3 +32,5 @@ for s, expected in testCases:
     answer = implementation.partitionLabels(s)
     if answer != expected:
         print(f"FAILED TEST: Expected {expected} but got {answer}. INPUT: {s}")
+
+print("Ran all tests")
