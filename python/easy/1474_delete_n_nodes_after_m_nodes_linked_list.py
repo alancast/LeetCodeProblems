4b7bbd484af5abb0eeebcc8e9c15fea030aaa0a7
Alex Lancaster <alancast@umich.edu>
2025-05-28 22:41:35 -0400

    Implement delete n nodes after m nodes linked list

---
+++ python/easy/1474_delete_n_nodes_after_m_nodes_linked_list.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    
+class Solution:
+    # Time O(len(list))
+    # Space O(1)
+    def deleteNodes(self, head: Optional[ListNode], m: int, n: int) -> Optional[ListNode]:        
+        pre_head = ListNode(next=head)
+
+        # Go through list until it's done
+        kept_count = 1
+        while head:
+            # Keeping
+            if kept_count < m:
+                kept_count += 1
+            # Time to skip some
+            else:
+                skipped_count = 0
+                while skipped_count < n and head.next:
+                    head.next = head.next.next
+                    skipped_count += 1
+                
+                kept_count = 1
+
+            head = head.next
+
+        return pre_head.next
