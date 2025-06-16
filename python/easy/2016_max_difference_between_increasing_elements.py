4e094c5fd3af6509645de4a528456ffafb20b185
Alex Lancaster <alancast@umich.edu>
2025-06-16 13:03:53 -0400

    Implement max difference between increasing elements

---
+++ python/easy/2016_max_difference_between_increasing_elements.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    # Time O(n) as we go through the list once
+    # Space O(1)
+    def maximumDifference(self, nums: List[int]) -> int:
+        max_diff = -1
+
+        lowest = float('inf')
+        for num in nums:
+            if num <= lowest:
+                lowest = num
+                continue
+
+            max_diff = max(max_diff, num - lowest)
+
+        return max_diff
+    
+test_cases = [
+    [4, [7,1,5,4]],
+    [-1, [9,4,3,2]],
+    [9, [1,5,2,10]],
+    [-1, [1,1,1]]
+]
+solution = Solution()
+for expected, nums in test_cases:
+    actual = solution.maximumDifference(nums)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: nums: {nums}")
+
+print("Ran all tests")
