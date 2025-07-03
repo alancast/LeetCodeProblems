60ee7be17992a8d53540d7fdba4e8afc3b5f31de
Alex Lancaster <alancast@umich.edu>
2025-07-03 10:28:23 -0400

    Implement find the kth character in a string game

---
+++ python/easy/3304_find_the_kth_character_in_string.py
@@ -0,0 +1,42 @@
+class Solution:
+    # Turns out you can math this
+    # Time O(logk)
+    # Space O(1)
+    def kthCharacter(self, k: int) -> str:
+        ans = 0
+        while k != 1:
+            t = k.bit_length() - 1
+            if (1 << t) == k:
+                t -= 1
+            k -= 1 << t
+            ans += 1
+
+        return chr(ord("a") + ans)
+    
+
+    # Brute force
+    def kthCharacter_brute_force(self, k: int) -> str:
+        word = "a"
+        char_map = {'z': 'a'}
+        while len(word) < k:
+            next_word = []
+            for char in word:
+                if char in char_map:
+                    next_word.append(char_map[char])
+                else:
+                    next_word.append(chr(ord(char) + 1))
+            word += ''.join(next_word)
+        
+        return word[k-1]
+    
+test_cases = [
+    ["b", 5],
+    ["c", 10]
+]
+solution = Solution()
+for expected, k in test_cases:
+    actual = solution.kthCharacter(k)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: k: {k}")
+
+print("Ran all tests")
