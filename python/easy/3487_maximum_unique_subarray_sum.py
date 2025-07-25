6f6e27d396db8faeebac5b37b613c64ce7b5ae82
Alex Lancaster <alancast@umich.edu>
2025-07-25 13:54:48 -0400

    Implement maximum unique subarray sum

---
+++ python/easy/3487_maximum_unique_subarray_sum.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    # Time O(n) as we go over full array once
+    # Space O(n) as array could be fully unique
+    def maxSum(self, nums: List[int]) -> int:
+        total = 0
+        max_min = float('-inf')
+        nums_set = set()
+
+        for num in nums:
+            if num in nums_set:
+                continue
+
+            # If all nums in set are neg we want the highest one
+            if num < 1:
+                max_min = max(max_min, num)
+                continue
+
+            nums_set.add(num)
+            total += num
+
+        # Had no positive numbers so return least negative
+        if total == 0:
+            return max_min
+        
+        return total
+    
+test_cases = [
+    [15, [1,2,3,4,5]],
+    [1, [1,1,0,1,1]],
+    [-1, [-1, -10]],
+    [3, [1,2,-1,-2,1,0,-1]]
+]
+solution = Solution()
+for expected, nums in test_cases:
+    actual = solution.maxSum(nums)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: nums: {nums}")
+
+print("Ran all tests")
