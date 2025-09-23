345405599222b85ac47640bd8f1a4a7adcd520cb
Alex Lancaster <alancast@umich.edu>
2025-09-23 11:25:47 -0400

    Update compareVersions as it's the question of day again

--- python/medium/165_compareVersions.py
+++ python/medium/165_compareVersions.py
@@ -6,7 +6,7 @@ class Solution:
         # if pointer is set to the end of string return 0
         if p > n - 1:
             return 0, p
-        
+
         # find the end of chunk
         p_end = p + 1
         while p_end < n and version[p_end] != '.':
@@ -14,23 +14,27 @@ class Solution:
 
         # retrieve the chunk
         i = int(version[p:p_end]) if p_end != n else int(version[p:n])
-        
+
         return i, p_end + 1
-        
+
+    # Time O(n) but done in one pass instead of 2
+    # Space O(n)
     def compareVersion(self, version1: str, version2: str) -> int:
         p1 = p2 = 0
         n1, n2 = len(version1), len(version2)
-        
+
         # compare versions
         while p1 < n1 or p2 < n2:
             i1, p1 = self.get_next_chunk(version1, n1, p1)
             i2, p2 = self.get_next_chunk(version2, n2, p2)            
             if i1 != i2:
                 return 1 if i1 > i2 else -1
-        
+
         # the versions are equal
         return 0
 
+    # Time O(n) 2 pass, one for split and one for compare
+    # Space O(n)
     def compareVersionTwoPass(self, version1: str, version2: str) -> int:
         v1 = version1.split('.')
         v1_size = len(v1)
@@ -45,23 +49,23 @@ class Solution:
                 return 1
             elif v2_int > v1_int:
                 return -1
-            
+
             i += 1
-        
+
         # Means v2 ended early
         while i < v1_size:
             v1_int = int(v1[i])
             if v1_int > 0:
                 return 1
-            
+
             i += 1
-        
+
         # Means v2 ended early
         while i < v2_size:
             v2_int = int(v2[i])
             if v2_int > 0:
                 return -1
-            
+
             i += 1
 
         return 0
