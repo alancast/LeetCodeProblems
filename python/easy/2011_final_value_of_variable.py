a252f4f520195c190f6c19d9601d728ce9eb70ea
Alex Lancaster <alancast@umich.edu>
2025-10-20 13:51:34 -0400

    Implement final value after operations

---
+++ python/easy/2011_final_value_of_variable.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    # Just go over all things in operations list
+    # Time O(n)
+    # Space O(1)
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        answer = 0
+        for op in operations:
+            if op[1] == '-':
+                answer -= 1
+            else:
+                answer += 1
+
+        return answer
+
+test_cases = [
+    [1, ["--X","X++","X++"]],
+    [3, ["++X","++X","X++"]],
+    [0, ["X++","++X","--X","X--"]]
+]
+solution = Solution()
+for expected, operations in test_cases:
+    actual = solution.finalValueAfterOperations(operations)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: operations: {operations}")
+
+print("Ran all tests")
