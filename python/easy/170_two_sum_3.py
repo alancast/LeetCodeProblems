2234579fc19f93910157b1ce359d81c7907b41cb
Alex Lancaster <alancast@umich.edu>
2025-11-08 09:33:08 -0500

    Implement two sum part 3

---
+++ python/easy/170_two_sum_3.py
@@ -0,0 +1,22 @@
+class TwoSum(object):
+
+    def __init__(self) -> None:
+        self.num_counts = {}
+
+    def add(self, number: int) -> None:
+        if number in self.num_counts:
+            self.num_counts[number] += 1
+        else:
+            self.num_counts[number] = 1
+
+    def find(self, value: int) -> bool:
+        # Go through all keys and see if other half is there
+        for num in self.num_counts.keys():
+            needed = value - num
+            if num != needed:
+                if needed in self.num_counts:
+                    return True
+            elif self.num_counts[num] > 1:
+                return True
+
+        return False
