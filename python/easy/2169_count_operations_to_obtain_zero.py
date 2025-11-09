4257690033ba468cdcc06e5b39e6e527e89ab8d4
Alex Lancaster <alancast@umich.edu>
2025-11-09 09:50:24 -0500

    Implement count operations to obtain zero

---
+++ python/easy/2169_count_operations_to_obtain_zero.py
@@ -0,0 +1,39 @@
+class Solution:
+    # Time O(logn)
+    # Space O(1)
+    def countOperations(self, num1: int, num2: int) -> int:
+        # total number of subtraction operations
+        operations = 0
+        while num1 and num2:
+            # each step of the Euclidean algorithm
+            operations += num1 // num2
+            num1 %= num2
+            num1, num2 = num2, num1
+
+        return operations
+
+    # Time O(logn)
+    # Space O(1)
+    def countOperations_simulatoin(self, num1: int, num2: int) -> int:
+        operations=0
+        while num1!=0 and num2!=0:
+            if num1>=num2:
+                num1=num1-num2
+            elif num2>=num1:
+                num2=num2-num1
+            operations+=1
+
+        return operations
+
+test_cases = [
+    [3, 2, 3],
+    [1, 10, 10]
+]
+solution = Solution()
+for expected, num1, num2 in test_cases:
+    actual = solution.countOperations(num1, num2)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}")
+        print(f"\tINPUTS: num1: {num1}, num2: {num2}")
+
+print("Ran all tests")
