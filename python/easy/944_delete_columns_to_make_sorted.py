90bd4474493dabe6ab4198ac4b91213f83260c7a
Alex Lancaster <alancast@umich.edu>
2025-12-20 08:54:30 -0500

    Implement delete columns to make sorted

---
+++ python/easy/944_delete_columns_to_make_sorted.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    # Just go over all words and see if column is deleted
+    # Time O(n*m)
+    # Space O(1)
+    def minDeletionSize(self, strs: List[str]) -> int:
+        rows = len(strs)
+        cols = len(strs[0])
+        answer = 0
+
+        for col in range(cols):
+            prev_char = strs[0][col]
+            for row in range(1, rows):
+                char = strs[row][col]
+                if char < prev_char:
+                    answer += 1
+                    break
+
+                prev_char = char
+
+        return answer
+
+test_cases = [
+    [1, ["bca", "daf", "ghi"]],
+    [0, ["a", "a"]],
+    [0, ["a", "b"]],
+    [3, ["zyx", "wvu", "tsr"]]
+]
+solution = Solution()
+for expected, strs in test_cases:
+    actual = solution.minDeletionSize(strs)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: strs: {strs}")
+
+print("Ran all tests")
