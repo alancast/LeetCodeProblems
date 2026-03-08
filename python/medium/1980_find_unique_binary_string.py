c5a7bd5e856756765f0392bbb216b9f3a98f0b9b
Alex Lancaster <alancast@umich.edu>
2026-03-08 09:39:21 -0400

    Lint todays daily of find unique binary string

--- python/medium/1980_find_unique_binary_string.py
+++ python/medium/1980_find_unique_binary_string.py
@@ -1,11 +1,9 @@
-from typing import List
-
 class Solution:
     # Change one digit of each entry
     # by time you are at end you are sure to have num different from all other nums
-    # Time O(n) where n is length of string. 
+    # Time O(n) where n is length of string.
     # Space O(1) hash set of full nums
-    def findDifferentBinaryString(self, nums: List[str]) -> str:
+    def findDifferentBinaryString(self, nums: list[str]) -> str:
         answer = []
         for i, num in enumerate(nums):
             if num[i] == '0':
@@ -17,10 +15,10 @@ class Solution:
 
     # Create integer hash set from nums and iterate til we get one that isn't there
     # Worst case scenario the nums are the first n binary nums
-    # Time O(n^2) where n is length of string. 
+    # Time O(n^2) where n is length of string.
     # We go through n of them and convert them to int which takes n time
     # Space O(n) hash set of full nums
-    def findDifferentBinaryString_slower(self, nums: List[str]) -> str:
+    def findDifferentBinaryString_slower(self, nums: list[str]) -> str:
         n = len(nums)
         num_set = set()
         for num in nums:
@@ -29,18 +27,20 @@ class Solution:
         # Guaranteed to get a number not included if we just go to n + 1
         for i in range(n + 1):
             if i not in num_set:
-                binary_str = bin(i)[2:].zfill(n)
-                return binary_str
-    
+                return bin(i)[2:].zfill(n)
+
+        # This will never happen, for linter, could throw here instead
+        return ""
+
 test_cases = [
-    [set(["00", "11"]), ["01", "10"]],
-    [set(["11", "10"]), ["00", "01"]],
-    [set(["000", "010", "100", "110", "101"]), ["111", "011", "001"]]
+    [{"00", "11"}, ["01", "10"]],
+    [{"11", "10"}, ["00", "01"]],
+    [{"000", "010", "100", "110", "101"}, ["111", "011", "001"]]
 ]
 solution = Solution()
-for expecteds, nums in test_cases:
+for expected, nums in test_cases:
     actual = solution.findDifferentBinaryString(nums)
-    if actual not in expecteds:
-        print(f"FAILED TEST! Expected {expecteds} but got {actual}. INPUTS: nums: {nums}")
+    if actual not in expected:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: nums: {nums}")
 
 print("Ran all tests")
