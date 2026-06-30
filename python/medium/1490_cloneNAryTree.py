2708950e8f7075beed2b3f5033e2d2a0d76f9255
Alex Lancaster <alancast@umich.edu>
2026-06-30 11:17:53 -0400

    Update comments on deep copy of N-ary Tree

--- python/medium/1490_cloneNAryTree.py
+++ python/medium/1490_cloneNAryTree.py
@@ -8,6 +8,9 @@ class Node:
         self.children = children if children is not None else []
 
 class Solution:
+    # BFS deep copy
+    # Time O(n)
+    # Space O(n)
     def cloneTree(self, root: Node) -> Node:
         if not root:
             return root
@@ -16,9 +19,11 @@ class Solution:
         # Starting point to kick off the BFS visits.
         queue = deque([(root, new_root)])
 
+        # Go over all nodes and just copy them
         while queue:
             old_node, new_node = queue.popleft()
 
+            # For every old child create a new child and add to queue
             for child_node in old_node.children:
                 new_child_node = Node(child_node.val)
                 new_node.children.append(new_child_node)
