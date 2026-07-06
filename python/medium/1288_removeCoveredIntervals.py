65f5e6dd3a717349bf817e56140e716e29f6d8cf
Alex Lancaster <alancast@umich.edu>
2026-07-05 21:47:36 -0400

    Update comments on remove covered intervals

--- python/medium/1288_removeCoveredIntervals.py
+++ python/medium/1288_removeCoveredIntervals.py
@@ -1,20 +1,29 @@
 class Solution:
+    # Greedy sort by start time and latest end time
+    # Time O(nlogn) for sort
+    # Space O(n)
     def removeCoveredIntervals(self, intervals: list[list[int]]) -> int:
         if not intervals:
             return 0
 
+        n = len(intervals)
+
+        # Sort by start time and then latest end time
         intervals = sorted(intervals, key= lambda x: (x[0], -x[1]))
-        count = 0
+        count_covered = 0
         previousEnd = -1
 
+        # Go over all intervals and see how many end before previous
+        # We know start will always increase
         for interval in intervals:
             end = interval[1]
             if end <= previousEnd:
-                count += 1
+                count_covered += 1
             else:
                 previousEnd = end
 
-        return len(intervals) - count
+        # Just subtract how many are covered from total
+        return n - count_covered
 
 testCases = [
     [[[1,4],[3,6],[2,8]], 2],
