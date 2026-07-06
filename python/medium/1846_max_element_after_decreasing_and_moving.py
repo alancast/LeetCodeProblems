391069061e4138aa7a98490c43b7476ede8af10a
Alex Lancaster <alancast@umich.edu>
2026-07-06 19:03:03 -0400

    Improve comments on max element after decreasing and moving items in an array

--- python/medium/1846_max_element_after_decreasing_and_moving.py
+++ python/medium/1846_max_element_after_decreasing_and_moving.py
@@ -1,16 +1,23 @@
 class Solution:
-    # Two pass
+    # Could sort and do in nlogn
+    # Or could do two pass and be O(n)
+    # First pass is counting how many of each number occur
+    # Second pass is see how high we can get number
     # Time O(n)
     # Space O(n)
     def maximumElementAfterDecrementingAndRearranging(self, arr: list[int]) -> int:
         n = len(arr)
-        counts = [0] * (n + 1)
 
+        # How many of each value occur
+        counts = [0] * (n + 1)
         for num in arr:
+            # Can't get above n as we need to start with 1. So cap at n
             counts[min(num, n)] += 1
 
+        # Second pass see how high we can get number to be
         answer = 1
         for num in range(2, n + 1):
+            # How high can we get by leveraging all the counts of this num
             answer = min(answer + counts[num], num)
 
         return answer
