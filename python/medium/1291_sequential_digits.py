6f60ae5590a3851fa5ebf3ed3c8b9766ca3edb7d
Alex Lancaster <alancast@umich.edu>
2026-07-17 17:35:31 -0400

    Update sequential digits implementation

--- python/medium/1291_sequential_digits.py
+++ python/medium/1291_sequential_digits.py
@@ -1,6 +1,22 @@
 class Solution:
+    # Sliding window over the digits and lengths
+    # Time O(64) outer loop 8, inner loop 8
+    # Space O(1)
     def sequentialDigits(self, low: int, high: int) -> list[int]:
-        return []
+        digits = "123456789"
+        answer = []
+
+        # Go from lowest possible length to highest possible
+        for length in range(len(str(low)), len(str(high)) + 1):
+            # Find starting char and see if num is above threshold
+            for start_digit in range(10 - length):
+                num = int(digits[start_digit: start_digit + length])
+
+                # Make sure this num is within the range then add it
+                if num >= low and num <= high:
+                    answer.append(num)
+
+        return answer
 
 test_cases = [
     [[123,234], 100, 300],
