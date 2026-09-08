76fb8690ec11ef816ae87f7cb0bf4e6064e7da61
Alex Lancaster <alancast@umich.edu>
2026-09-08 10:58:46 -0400

    Implement count commas in a range

---
+++ python/easy/3870_count_commas_in_range.py
@@ -0,0 +1,26 @@
+class Solution:
+    # Just do math
+    # Time O(1)
+    # Space O(1)
+    def countCommas(self, n: int) -> int:
+        if n < 1000:
+            return 0
+        # Every number in this range has one comma so just subtract first 999
+        if n < 1000000:
+            return n - 999
+
+        # Bounds make this the last return amount
+        # First 100k have some amount, next all have 2 commas
+        return (1000000 - 999) + ((n - 999999) * 2)
+
+test_cases = [
+    [3, 1002],
+    [0, 998]
+]
+solution = Solution()
+for expected, n in test_cases:
+    actual = solution.countCommas(n)
+    if expected != actual:
+        print(f"FAILED TEST! Expected {expected} but got {actual}. INPUTS: n: {n}")
+
+print("Ran all tests")
